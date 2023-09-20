035323c98679fc4602b8852247056970c672c2c5
imtiazuddin4616 <138401314+imtiazuddin4616@users.noreply.github.com>
2023-09-20 18:56:22 +0600

    Add files via upload

--- Javascript_loop.js
+++ Javascript_loop.js
@@ -1,4 +1,5 @@
 console.log("for loop");
+
 for(let i=1;i<=10;i++){
   console.log(i);
 }
@@ -24,3 +25,12 @@ console.log(x);
 x++;
 
 }while(x<=10);
+
+console.log("for in loop");
+
+let cars={name:"BMW",model:"x5",price:10000000};
+for(let x in cars){
+  console.log(x);
+  console.log(cars[x]);
+}
+
