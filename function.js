5ee442a309298cd5a8e67eb61ed77548eb3c73d9
imtiazuddin4616 <138401314+imtiazuddin4616@users.noreply.github.com>
2023-09-20 20:44:21 +0600

    Add files via upload

--- function.js
+++ function.js
@@ -1,13 +1,6 @@
-function number(a,b,c){
-  console.log(a,b,c);
+function num(a,b,c){
+  num=a*b*c;
+  return num;
 
 }
-number(1,2,3); 
-
-console.log(-------);
-
-function number(a,b,c){
-  let x=a*b+c;
-  console.log(x);
-}
-number(1,2,4);
+console.log(num(2,2,2));
