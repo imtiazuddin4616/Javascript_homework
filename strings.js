386924ceee8effddb26207a9b0ea1b4c535541b0
Imtiaz <138401314+imtiazuddin4616@users.noreply.github.com>
2023-09-21 16:11:41 +0600

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,8 @@
+let text="this is a very \'speacial\' text";
+let text1="this is a very \"speacial\" text";
+let text2="it\'s a very speacial text";
+let text3="This is a very \n speacial text";
+console.log(text);
+console.log(text1);
+console.log(text2);
+console.log(text3);
