d89a20e768233602580399b2dc2dc9aa6377499a
Imtiaz <138401314+imtiazuddin4616@users.noreply.github.com>
2023-09-21 17:53:41 +0600

    Add files via upload

---
+++ strings_method.js
@@ -0,0 +1,18 @@
+
+let x="imtiaz uddin raju";
+console.log(x.length);
+console.log(x.slice(0,6));
+console.log(x.slice(-17,-10));
+console.log(x.substring(7,13));
+console.log(x.substr(7,9))
+console.log(x.replace("raju","boss"));
+console.log(x.toUpperCase());
+console.log(x.toLowerCase());
+console.log(x.charAt(0));
+console.log(x.split(","));
+console.log(x.split(" "));
+console.log(x.split(""));
+let y="i love cats,cats are so good,cats are so cute";
+console.log(y.replaceAll("cats","Dogs"));
+let z="      imtiaz     ";
+console.log(z.trim());
