c5729753a38a562e1ec088258dc8be177bba77a0
Imtiaz <138401314+imtiazuddin4616@users.noreply.github.com>
2023-09-23 04:14:39 +0600

    Add files via upload

---
+++ string_template.js
@@ -0,0 +1,14 @@
+for(i=1;i<=10;i++){
+  console.log(`5*${i}=${i*5}`);
+}
+
+let name="imtiaz";
+console.log(`my name is ${name}`);
+
+let num1=15;
+let num2=10;
+console.log(`the sum is ${num1+num2}`);
+
+let text=`my name is "imtiaz."
+i am a very good 'developer.'`;
+ console.log(text);
