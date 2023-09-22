a31bbfe404caab9a8e808ef938b0b63c8bfb6259
Imtiaz <138401314+imtiazuddin4616@users.noreply.github.com>
2023-09-23 02:35:50 +0600

    Add files via upload

--- string_search.js
+++ string_search.js
@@ -9,3 +9,7 @@ console.log(names.lastIndexOf("imtiaz")); //last index of a string
 console.log(names.indexOf("imtiaz",16)); //searching a index number after a specific index number
 console.log(name.indexOf("boss")); //-1 means the string we are looking for are not there
 console.log(name.includes("imtiaz"));
+let name3="imtiaz uddin raju";
+console.log(name3.endsWith("uddin",12));
+let name2="imtiaz uddin raju";
+console.log(name2.startsWith("uddin",7));
