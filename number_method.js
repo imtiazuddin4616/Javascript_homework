92eef4576fdb41b45fd369a1c977d2673662478b
Imtiaz <138401314+imtiazuddin4616@users.noreply.github.com>
2023-09-23 18:12:53 +0600

    Add files via upload

---
+++ number_method.js
@@ -0,0 +1,9 @@
+let x=123;
+console.log(x.toString());
+console.log((123).toString());
+console.log(typeof x.toString());
+
+let y=1.662;
+console.log(y.toFixed());
+let z=1.934;
+console.log(z.toPrecision(2));
